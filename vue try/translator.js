d9006123e3cc0501aec057c9067c01b02f304642
YuxingLu613 <1293327174@qq.com>
2021-06-15 20:05:17 +0800

    fix translator

--- vue try/translator.js
+++ vue try/translator.js
@@ -1,69 +1,67 @@
-var translator = new Vue( {
-    el:"#translator",
-    data:{
-        lanindex:['zh',"en","Japanese"],
-        languages: [{
-          value: 'zh',
-          label: '中文',
-          disabled:false
-        }, {
-          value: 'en',
-          label: '英文',
-          disabled:false
-        }, {
-          value: 'Japanese',
-          label: '日语',
-          disabled:false
-        }, {
-          value: '选项4',
-          label: '龙须面',
-          disabled:false
-        }, {
-          value: '选项5',
-          label: '北京烤鸭',
-          disabled:false
-        }],
-        from:null,
-        to:null,
-        textarea:"",
-        result:""
-      },
-    methods:{
-        getfrom:function(event){
-            var that=this;
-            for(var i=0;i<that.languages.length;i++){
-                that.languages[i].disabled=false;
-            }
-            console.log("from "+that.from);
-            this.languages[that.lanindex.indexOf(event)].disabled=true;
+var translator = new Vue({
+  el: "#translator",
+  data: {
+    lanindex: ['zh', "en", "Japanese"],
+    languages: [{
+      value: 'zh',
+      label: '中文',
+      disabled: false
+    }, {
+      value: 'en',
+      label: '英文',
+      disabled: false
+    }, {
+      value: 'ja',
+      label: '日语',
+      disabled: false
+    },],
+    from: null,
+    to: null,
+    textarea: "",
+    result: ""
+  },
+  methods: {
+    getfrom: function (event) {
+      var that = this;
+      for (var i = 0; i < that.languages.length; i++) {
+        that.languages[i].disabled = false;
+      }
+      console.log("from " + that.from);
+      this.languages[that.lanindex.indexOf(event)].disabled = true;
+    },
+    getto: function (event) {
+      var that = this;
+      for (var i = 0; i < that.languages.length; i++) {
+        that.languages[i].disabled = false;
+      }
+      console.log("to " + that.to);
+      that.languages[that.lanindex.indexOf(event)].disabled = true;
+    },
+    translate: function () {
+      var that = this;
+      console.log("wait to translate: " + that.textarea);
+      that.token = "fpag1ki0f4oe7fs5sqys";
+      quary = "http://api.interpreter.caiyunai.com/v1/translator"
+      data = {
+        "source": that.textarea,
+        "trans_type": "auto2"+that.to,
+        "request_id": "demo",
+        "detect": true,
+      };
+      headers = {
+        'content-type': "application/json",
+        'x-authorization': "token " + that.token,
+      }
+      axios.post(quary, data, {
+        headers: headers
+      }).then(
+        function (response) {
+          console.log(response);
+          that.result = response.data.target
         },
-        getto:function(event){
-            var that=this;
-            for(var i=0;i<that.languages.length;i++){
-                that.languages[i].disabled=false;
-            }
-            console.log("to "+that.to);
-            that.languages[that.lanindex.indexOf(event)].disabled=true;
-        },
-        translate:function(){
-            var that=this;
-            console.log("wait to translate: "+that.textarea)
-            console.log("20210527000844094"+that.textarea+"1435660288osnzIH44YDlLl8UpI6_L")
-            sign=md5("20210527000844094"+that.textarea+"1435660288osnzIH44YDlLl8UpI6_L")
-            console.log(sign)
-            quary="https://fanyi-api.baidu.com/api/trans/vip/translate?q="+that.textarea+"&from="+that.from+"&to="+that.to+"&appid=20210527000844094&salt=1435660288&sign="+sign
-            axios.get(quary,{headers: {
-              "Access-Control-Allow-Origin": "*",
-              "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
-              "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token"
-            }}).then(
-                function(response){
-                    console.log(response);
-                    that.result=response.data
-                },
-                function(error){}
-            )
+        function (error) {}
+      )
 
-        }
     }
-  })
+  }
+})
